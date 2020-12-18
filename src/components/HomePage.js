57af7b2238ce9f241ecf3e1e61e2cc26f4837d9a
jadedragon21 <74530173+jadedragon21@users.noreply.github.com>
2020-12-18 12:45:13 -0600

    routertest slide working

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>Here is a description of our restaurant!</p>
+        <img src="https://i0.wp.com/68.media.tumblr.com/159d31bca61108d5bd1a8dedf5c14dfe/tumblr_otlasg917I1ql7xb0o6_1280.gif?" alt="Kyle MacLachlan eating Mashed Potatoes"/>
+      </div>
+    );
+  }
+}
+export default HomePage;
